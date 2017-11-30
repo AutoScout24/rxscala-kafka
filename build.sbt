22eecfe64a7c81f25a7fcb344a46f80f1b2cc570
Conor Brady <conor.brady@scout24.com>
2017-11-30 14:19:55 +0100

    Conor - AS24-27723 - Initial commit, setup sbt, added .gitignore, added KafkaTopicSyncOnSubscribe

---
+++ build.sbt
@@ -0,0 +1,42 @@
+
+name := "rxscala-kafka"
+organization := "com.autoscout24"
+organizationName := "AutoScout24"
+organizationHomepage := Some(url("https://www.autoscout24.de/"))
+
+version := "0.1"
+
+scalaVersion := "2.12.4"
+
+scalacOptions in ThisBuild ++= Seq("-unchecked", "-deprecation", "-feature", "-Xfatal-warnings")
+
+javacOptions ++= Seq("-source", "1.8", "-target", "1.8", "-Xlint")
+
+javaOptions in (ThisBuild, Test) ++= Seq(
+  "-Dconfig.resource=test.conf",
+  "-Dlogback.configurationFile=as24local-logger.xml"
+)
+fork in Test := true
+
+val kafkaVersion = "0.10.1.1"
+val awsSdkVersion = "1.11.128"
+val sl4jVersion = "1.7.21"
+
+resolvers += Resolver.jcenterRepo
+libraryDependencies ++= Seq(
+  "org.slf4j" % "slf4j-api" % sl4jVersion,
+  "org.slf4j" % "log4j-over-slf4j" % sl4jVersion, // Kafka uses log4j
+  "org.scalatest"  %% "scalatest" % "3.0.1" % Test,
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.5.0",
+  "ch.qos.logback" % "logback-classic" % "1.2.3",
+  "net.logstash.logback" % "logstash-logback-encoder" % "4.11",
+  "com.typesafe" % "config" % "1.3.0",
+  "io.reactivex" % "rxscala_2.11" % "0.26.2",
+  "com.fasterxml.jackson.dataformat" % "jackson-dataformat-cbor" % "2.8.5",
+  "org.eclipse.jetty" % "example-jetty-embedded" % "9.3.6.v20151106" exclude("org.eclipse.jetty.tests", "test-mock-resources"),
+  "org.apache.kafka" % "kafka-clients" % kafkaVersion,
+)
+
+// Publishing logic goes here
+lazy val publishSettings = Seq()
+        
---
+++ src/main/scala/logging/TypedLog.scala
@@ -0,0 +1,89 @@
+package com.autoscout24.gecloud.gaspedaal.exporter.logging
+
+import com.typesafe.scalalogging.Logger
+import TypedLog.StringHelper
+
+import scala.language.implicitConversions
+import net.logstash.logback.marker.{LogstashMarker, Markers}
+
+import scala.collection.JavaConverters._
+
+sealed trait TypedLog extends Product {
+
+  /**
+    * Require all logs to have a standard message field
+    */
+  val message: String
+
+  /**
+    * store the `type` as string for the current log
+    */
+  final val typeName: String = StringHelper.camelToHyphens(this.getClass.getSimpleName)
+
+  def toMap: Map[String, Any] =
+    this.getClass.getDeclaredFields
+      .map(_.getName) // all field names
+      .zip(this.productIterator.to)
+      .toMap // zipped with all values
+
+}
+
+object TypedLog {
+  case class KafkaConsumerStarted(topic: String) extends TypedLog {
+    override val message: String = s"Kafka consumer started for the topic $topic"
+  }
+
+  case class KafkaConsumerStopped(topic: String) extends TypedLog {
+    override val message: String = s"Kafka consumer stopped for the topic $topic"
+  }
+
+  case class KafkaPartitionsAssigned(topic: String) extends TypedLog {
+    override val message: String = s"Kafka partitions assigned for the topic $topic"
+  }
+
+  case class KafkaPartitionsRevoked(topic: String) extends TypedLog {
+    override val message: String = s"Kafka partitions revoked for the topic $topic"
+  }
+
+  case class OffsetCommitFailed(topic: String, e: Throwable) extends TypedLog {
+    override val message: String = s"Failed to commit offsets for the topic $topic"
+  }
+
+  /**
+    * Extesion method: Extend regular logback logger
+    */
+  implicit class LoggerEnhancer(logger: Logger) {
+
+    private def typedLogToMarkerConverterExcludingMessage(typedLog: TypedLog): LogstashMarker =
+      Markers.appendEntries(
+        (
+          typedLog.toMap
+            .filterKeys(_ != "message") + ("type" -> typedLog.typeName) // add `type` field to list of values
+          ).asJava
+      )
+
+    def trace(typedLog: TypedLog): Unit =
+      logger.trace(typedLogToMarkerConverterExcludingMessage(typedLog), typedLog.message)
+    def debug(typedLog: TypedLog): Unit =
+      logger.debug(typedLogToMarkerConverterExcludingMessage(typedLog), typedLog.message)
+    def info(typedLog: TypedLog): Unit =
+      logger.info(typedLogToMarkerConverterExcludingMessage(typedLog), typedLog.message)
+    def warn(typedLog: TypedLog): Unit =
+      logger.warn(typedLogToMarkerConverterExcludingMessage(typedLog), typedLog.message)
+    def error(typedLog: TypedLog): Unit =
+      logger.error(typedLogToMarkerConverterExcludingMessage(typedLog), typedLog.message)
+
+    def error(typedLog: TypedLog, cause: Throwable): Unit =
+      logger.error(typedLogToMarkerConverterExcludingMessage(typedLog), typedLog.message, cause)
+  }
+
+  object StringHelper {
+    def camelToHyphens(string: String) =
+      "[A-Z\\d]".r
+        .replaceAllIn(string, { m =>
+          "-" + m.group(0).toLowerCase()
+        })
+        .stripPrefix("-")
+  }
+
+}
